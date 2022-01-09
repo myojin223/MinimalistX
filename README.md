3a03b40e400106bcd26d2a07ed07d74404227a00
myojin223 <64243555+myojin223@users.noreply.github.com>
2022-01-09 03:24:57 -0700

    Initial upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Drag and drop the folder inside of the "Minimalist-main" folder into your existing Proximity template directory
+
+## For Proximity 0.5.4+
---
+++ minimalist/Minimalist.bat
@@ -0,0 +1,2 @@
+java -jar proximity.jar --cards=Decklist.txt --art_source=BEST --reminder_text=true --template=minimalist --copyright=false --set_symbol=pz1 --use_card_back=false --watermark=NONE
+cmd /k
